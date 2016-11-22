05d8dd8075bc61619236487738d5fa09c135a463
paolocode <paolo.codenotti@gmail.com>
2016-11-21 20:50:44 -0800

    Add files via upload

--- utils/csv_util_test.cc
+++ utils/csv_util_test.cc
@@ -7,7 +7,7 @@
 
 bool CheckEqualsString(const std::string& str1, const std::string& str2) {
   if (str1 != str2) {
-    std::cerr << "Expected " << str1 << "==" << str2; 
+    std::cerr << "Expected strings: '" << str1 << "' == '" << str2 << "'" << std::endl;
     return false;
   }
   return true;
@@ -15,7 +15,7 @@ bool CheckEqualsString(const std::string& str1, const std::string& str2) {
 
 bool CheckEqualsInt(const int i1, const int i2) {
   if (i1 != i2) {
-    std::cerr << "Expected " << i1 << "==" << i2; 
+    std::cerr << "Expected Ints: " << i1 << " == " << i2 << std::endl;
     return false;
   }
   return true;
@@ -25,22 +25,27 @@ int main(int argc, char** argv) {
   bool test_passed = true;
   std::ifstream file("test.csv");
   if (!file.is_open()) {
-    std::cerr << "FAILED TO OPEN FILE";
+    std::cerr << "FAILED TO OPEN FILE" << std::endl;
     return 2;
   }
   std::vector<std::string> tokens = getNextLineAndSplitIntoTokens(file);
   if (!CheckEqualsInt(tokens.size(), 4)) test_passed = false;
   if (!CheckEqualsString(tokens[0], "I am") ||
-      !CheckEqualsString(tokens[1], "a test") ||
-      !CheckEqualsString(tokens[2], "csv") ||
-      !CheckEqualsString(tokens[3], "file")) test_passed = false;
+      !CheckEqualsString(tokens[1], " a test") ||
+      !CheckEqualsString(tokens[2], " csv") ||
+      !CheckEqualsString(tokens[3], " file")) test_passed = false;
 
   tokens = getNextLineAndSplitIntoTokens(file);
   if (!CheckEqualsInt(tokens.size(), 3)) test_passed = false;
   if (!CheckEqualsString(tokens[0], "I am") ||
-      !CheckEqualsString(tokens[1], "number 1") ||
-      !CheckEqualsString(tokens[2], "test!")) test_passed = false;
+      !CheckEqualsString(tokens[1], " number 1") ||
+      !CheckEqualsString(tokens[2], " test!")) test_passed = false;
 
-  if (test_passed) return 0;
-  else return 2;
+  if (test_passed) {
+    std::cout << "SUCCESS!" << std::endl;
+    return 0;
+  } else  {
+    std::cout << "TEST FAILED!" << std::endl;
+    return 2;
+  }
 }
