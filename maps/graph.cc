779f7d1d2625668e857dff4236f2a1e87bdb9204
paolocode <paolo.codenotti@gmail.com>
2016-11-29 00:58:51 -0800

    Add files via upload

--- maps/graph.cc
+++ maps/graph.cc
@@ -2,7 +2,9 @@
 
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <math.h>
+#include <stdlib.h>
 #include <string>
 #include <vector>
 
@@ -22,14 +24,19 @@ std::string CardinalDirectionToString(const CardinalDirection cd) {
   switch (cd) {
     case NORTH:
       return "N";
+      break;
     case EAST:
       return "E";
+      break;
     case SOUTH:
       return "S";
+      break;
     case WEST:
       return "W";
+      break;
     case ERROR:
       return "ERROR";
+      break;
     default:
       return "UNKOWN DIRECTION";
   }
@@ -37,7 +44,7 @@ std::string CardinalDirectionToString(const CardinalDirection cd) {
 
 // Returns the number of unparseable edges (or -1 if error opening the file);
 int Graph::BuildFromCSVFile(const std::string& filename) {
-  std::ifstream file(filename);
+  std::ifstream file(filename.c_str());
   if (!file.is_open()) {
     std::cerr << "FAILED TO OPEN FILE" << std::endl;
     return -1;
@@ -65,47 +72,158 @@ int Graph::BuildFromCSVFile(const std::string& filename) {
 bool Graph::AddEdge(const std::vector<std::string> &tokens) {
   if (tokens.size() != 16) return false;
   // Parse all tokens (we could put them directly in, but this way we know what they are.
-  const float length = std::stof(tokens[0]);
-  const int from_cross_street_id = static_cast<int>(std::stof(tokens[1]));
-  const std::string &street_name = tokens[3] + " " + tokens[2];
+  const float length = atof(tokens[0].c_str());
+  const int from_cross_street_id = static_cast<int>(atof(tokens[1].c_str()));
+  const std::string &street_name = tokens[3] + " " + tokens[2] + " " +
+    tokens[10];
   const std::string &zip_code = tokens[4];
-  const int from_node_id = static_cast<int>(std::stof(tokens[5]));
+  const int from_node_id = static_cast<int>(atof(tokens[5].c_str()));
   const std::string &dir_travel = tokens[6]; // TODO(paolo): actually encode directionality.
   const std::string &from_cross_street = tokens[7];
   const std::string &censusubl = tokens[8];
-  const int to_node_id = static_cast<int>(std::stof(tokens[9]));
+  const int to_node_id = static_cast<int>(atof(tokens[9].c_str()));
   const std::string &street_type = tokens[10];
   const CardinalDirection ewns_dir = CardinalDirectionFromString(tokens[11]);
   // b = beginning, e = end for latlong?
-  const double &blong = std::stod(tokens[12]);
-  const double &elong = std::stod(tokens[13]);
-  const double &blat = std::stod(tokens[14]);
-  const double &elat = std::stod(tokens[15]);
+  const double &blong = atof(tokens[12].c_str());
+  const double &elong = atof(tokens[13].c_str());
+  const double &blat = atof(tokens[14].c_str());
+  const double &elat = atof(tokens[15].c_str());
 
   // Create the edge and populate values.
-  const Edge edge = {from_node_id, to_node_id, length, ewns_dir};
-  // Check if the from_node already exists. If not, create it.
-  if (from_node_id > nodes_.size()) {
-    nodes_.resize(from_node_id + 1);
+  const Edge edge = {from_node_id, to_node_id, length, ewns_dir,
+                     street_name + " & " + from_cross_street};
+  const LatLong from_latlong = {blat, blong};
+  if (!AddNodeIfNotPresent(from_node_id, from_latlong,
+			   street_name + " & " + from_cross_street))
+    return false;
+  nodes_[from_node_id].edges_.push_back(edge);
+  const LatLong to_latlong = {elat, elong};
+  if (!AddNodeIfNotPresent(to_node_id, to_latlong,
+			   street_name + " & " + from_cross_street))
+    return false;
+  // Add to_edge and inverse if needed.
+  Node *to_node = &nodes_[to_node_id];
+  if (!to_node->HasEdgeTo(from_node_id)) {
+    const Edge reverse_edge = {to_node_id, from_node_id, length, ewns_dir,
+			       "I:" + street_name + " & " + from_cross_street};
+    to_node->edges_.push_back(reverse_edge);
   }
-  Node *node = &nodes_[from_node_id];
-  if (node->index_ == -1) {  // This node is not inialized yet!
-    const LatLong from_latlong = {blat, blong};
-    node->Initialize(from_node_id, from_latlong,
-		     street_name + " & " + from_cross_street);
-  }
-  node->edges_.push_back(edge);
   return true;
 }
 
-void PrintEdge (const Edge &edge, int offset = 0) {
+std::string LatLongDebugString(LatLong latlong) {
+  return "(" + std::to_string(latlong.lat) + "," +
+    std::to_string(latlong.lon) + ")";
+}
+
+bool Graph::AddNodeIfNotPresent(int node_id, LatLong latlong,
+				std::string name) {
+  // Check if the from_node already exists. If not, create it.
+  if (node_id > kMaxNumNodes || node_id < 0) return false;
+
+  if ((unsigned)node_id > nodes_.size()) {
+    nodes_.resize(node_id + 1);
+  }
+  Node *node = &nodes_[node_id];
+  if (node->index_ == -1) {  // This node is not inialized yet!
+    node->Initialize(node_id, latlong, name);
+  }// else if (node->latlong_.lat != latlong.lat ||
+  //	     node->latlong_.lon != latlong.lon) {
+  //    node->other_latlongs_ = node->other_latlongs_ + LatLongDebugString(latlong);
+  //  }
+  return true;
+}
+
+bool Node::HasEdgeTo(const int id) const {
+  for (int i = 0; i < edges_.size(); ++i) 
+    if (edges_[i].to_id == id) return true;
+  return false;
+}
+
+void Node::Print(const bool verbose) const {
+  if (verbose) {
+    std::cout << "Node index " << index_ << " :" << std::endl;
+    std::cout << " <" << std::endl;
+    std::cout << "  intersection: " << intersection_ << std::endl;
+    std::cout << "  latlong: " << latlong_.lat << "," << latlong_.lon << std::endl;
+    for (int i = 0; i < edges_.size(); ++i) PrintEdge(edges_[i], verbose, 2);
+    std::cout << " >" << std::endl;
+  } else {
+    std::cout << "idx " << index_
+	      << "(" << latlong_.lat << "," << latlong_.lon << "):";
+    for (int i = 0; i < edges_.size(); ++i) PrintEdge(edges_[i], verbose, 2);
+    std::cout << std::endl;
+  }
+}
+
+void PrintEdge (const Edge &edge, const bool verbose, int offset) {
   const std::string off (offset, ' ');
-  std::cout << off << edge.from_id << " -> " << edge.to_id << std::endl;
-  std::cout << off << "  <" << std::endl;
-  std::cout << off << "   length: " << edge.length << std::endl;
-  std::cout << off << "   direction: " << CardinalDirectionToString(edge.direction) << std::endl;
-  std::cout << off << "   street_name: " << edge.street_name << std::endl;
-  std::cout << off << "  >" << std::endl;
+  if (verbose) {
+    std::cout << off << edge.from_id << " -> " << edge.to_id << std::endl;
+    std::cout << off << "  <" << std::endl;
+    std::cout << off << "   length: " << edge.length << std::endl;
+    std::cout << off << "   direction: "
+	      << CardinalDirectionToString(edge.direction) << std::endl;
+    std::cout << off << "   street_name: " << edge.street_name << std::endl;
+    std::cout << off << "  >" << std::endl;
+  } else {
+    std::cout << off << edge.from_id << "->" << edge.to_id << "{" 
+	      << edge.street_name << ", len: " << edge.length << " dir: "
+	      << CardinalDirectionToString(edge.direction) << "} ";
+  }
+}
+
+void Graph::PrintInternal(const bool verbose, int max_num) const {
+  if (max_num == -1 || max_num > nodes_.size())
+    max_num = nodes_.size();
+  for (int i = 0; i < max_num; ++i) {
+    if (nodes_[i].index_ != -1) nodes_[i].Print(verbose);
+  }
+}
+
+void Graph::PrintStats(bool verbose) const {
+  std::cout << "Number of vertices: " << nodes_.size() << std::endl;
+  int num_uninitialized_vertices = 0;
+  int num_nodes_multiple_latlongs = 0;
+  int num_edges = 0;
+  std::map<int, int> degree_hist;
+  int max_degree = -1;
+  int max_degree_index = 0; 
+  for (const auto& node : nodes_) {
+    if (node.index_ == -1) {
+      ++num_uninitialized_vertices;
+      continue;
+    }
+    if (!node.other_latlongs_.empty()) {
+      ++num_nodes_multiple_latlongs;
+    }
+    const int& degree = node.edges_.size();
+    degree_hist[degree]++;
+    num_edges += degree;
+    if (degree > max_degree) {
+      max_degree = degree;
+      max_degree_index = node.index_;
+    }
+  }
+  std::cout << "Number of good (initalized) vertices: "
+	    << nodes_.size() - num_uninitialized_vertices << std::endl;
+  std::cout << "Number of vertices with multiple latlongs: "
+	    << num_nodes_multiple_latlongs << std::endl;
+  std::cout << "Number of edges: " << num_edges << std::endl;
+  std::cout << "Maximum degree: " << max_degree << std::endl;
+  if (verbose) {
+    std::cout << "max_degree vertex: ";
+    nodes_[max_degree_index].Print();
+  }
+  //  std::cout << "median_degree" << GetMedianFromHist(degree_hist);
+  if (verbose) {
+    std::cout << "Full degree histogram: degree -> num_nodes: " << std::endl;
+    for (std::map<int, int>::iterator it = degree_hist.begin(); 
+	 it!= degree_hist.end(); ++it) {
+      std::cout << it->first << " --> " << it->second << std::endl;
+    }
+  }
 }
 
 }  // namespace graph
--- maps/graph.h
+++ maps/graph.h
@@ -5,6 +5,8 @@
 
 namespace graph {
 
+const int kMaxNumNodes = 1000000;
+
 enum CardinalDirection {
   ERROR = 0,
   NORTH = 1,
@@ -26,14 +28,19 @@ typedef struct {
   std::string street_name;
 } Edge;
 
+void PrintEdge (const Edge &edge, const bool verbose, int offset = 0);
+
 class Node {
 public:
   Node() { index_ = -1; }
-  void Initialize(const int index, const LatLong &latlong, const std::string &intersection) {
-    index_ = index;
+  void Initialize(const int index, const LatLong &latlong,
+		  const std::string &intersection) {
+    index_ = index; 
     latlong_ = latlong;
     intersection_ = intersection;
   }
+  bool HasEdgeTo(const int id) const;
+  void Print(const bool verbose = false) const;
   int index_;
   LatLong latlong_;
   // Can we get this in canonical form?
@@ -41,15 +48,26 @@ public:
   // Indices of neighbors in the nodes vector in Graph.
   std::vector<Edge> edges_;
   // Should this really live inside the Node class?
-  std::vector<int> neighborhood_30_min;
+  std::vector<int> neighborhood_30_min_;
+  std::string other_latlongs_;
 };
 
 class Graph {
  public:
   ~Graph() { };
   int BuildFromCSVFile(const std::string &filename);
+  void PrintVerbose(int max_num = kMaxNumNodes) const {
+    PrintInternal(/*verbose =*/true, max_num);
+  }
+  void Print(int max_num = kMaxNumNodes) const {
+    PrintInternal(/*verbose =*/false, max_num);
+  } 
+  void PrintStats(bool verbose = false) const;
  private:
+  void PrintInternal(const bool verbose, int max_num) const;
   bool AddEdge(const std::vector<std::string> &tokens);
+  bool AddNodeIfNotPresent(int node_id, LatLong lat_long,
+			   std::string name);
   std::vector<Node> nodes_;
 };
 
